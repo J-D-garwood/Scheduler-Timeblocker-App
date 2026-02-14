7f24f5d1269c0850106b3776855258854613698e
J_D_Nye <garwoodjdm@gmail.com>
2026-02-14 13:00:32 +1100

    added add project form (rough)

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+function openaddprojectForm () {
+    document.getElementById("addProjectForm").style.display = "block";
+}
+
+function closeaddprojectForm() {
+  document.getElementById("addProjectForm").style.display = "none";
+}
